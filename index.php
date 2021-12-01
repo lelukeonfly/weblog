73dadce6e52c531931bd0d95c028612af4d632af
lelukeonfly@gmail.com <lelukeonfly@gmail.com>
2021-12-01 11:28:21 +0100

    removed debug on index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
     // In Blogs werden Einträge immer in umgekehrter Reihenfolge angezeigt
     $unserialized = hole_eintraege(true);
-    var_dump($_SESSION);
+    #var_dump($_SESSION);
 ?>
 <!DOCTYPE html>
 <html>
